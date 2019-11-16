c588929ee2926d47431ddae0342e81d9de210ab3
Jessie S <jessie.sholler@gmail.com>
2019-11-16 10:49:53 -0500

    images and links showing up

--- client/src/components/SearchResults/index.js
+++ client/src/components/SearchResults/index.js
@@ -1,17 +1,25 @@
 import React from "react"; 
-// import { TableRow } from '@material-ui/core';
+
+import { TableRow, TableCell } from '@material-ui/core';
 // import { getThemeProps } from "@material-ui/styles";
 
 function SearchResults(props) {
 
     return (
+        
         props.books.map((book, i)=> {
-           return  <div key={i}> Search results
-            {book.id}
-            {book.title} </div>
+           return  <TableRow key={i}> 
+            <TableCell>{book.id}</TableCell> 
+            <TableCell style={{height: 100, width: 100 }}><img src={book.image} /></TableCell>
+            <TableCell>
+               <h3>{book.title}</h3> 
+               <p> {book.description}</p>
+               <a href={book.link}>Click to view book</a>
+            </TableCell> 
+            </TableRow>
         })
 
     )
-}
+} 
 
 export default SearchResults; 
